c6d448cb85bfb964aa58f14012c91cbe7fd5448a
GhoustEr <47031401+GhoustEr@users.noreply.github.com>
2019-02-11 04:55:46 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : DREAM ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`You Still Dead`,"http://twitch.tv/Dream")
+client.user.setGame(`Iam Here For You`,"http://twitch.tv/Dream")
 client.user.setStatus("dnd")
 });
 
